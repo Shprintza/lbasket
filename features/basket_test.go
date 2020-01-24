629bb74fa7ad2925e49f5819cb59073dcba1a3fb
javi <hi@orov.io>
2020-01-25 00:33:50 +0100

    Start & Down server with cucumber if needed

--- features/basket_test.go
+++ features/basket_test.go
@@ -1,9 +1,16 @@
 package features
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"log"
+	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/DATA-DOG/godog"
+	"github.com/briandowns/spinner"
 	"github.com/google/uuid"
 	"github.com/orov-io/lbasket/client"
 	"github.com/orov-io/lbasket/models"
@@ -105,19 +112,146 @@ func FeatureContext(s *godog.Suite) {
 	s.Step(`^I try to retrive the basket$`, iCallToGetTheValidBasket)
 	s.Step(`^I should receive desired basket$`, iShouldReceiveDesiredBasket)
 
+	wasRunning := false
 	s.BeforeSuite(func() {
-		upServer()
+		if !serviceAlreadyRunning() {
+			upServer()
+			return
+		}
+		fmt.Print("Server was running before run tests!")
+		wasRunning = true
 	})
 
 	s.AfterSuite(func() {
-		downServer()
+		if !wasRunning {
+			downServer()
+		}
+		fmt.Println("Leaving server living")
 	})
 }
 
+const dockerComposeTimeOut = 120
+const dockerCompose = "docker-compose"
+const useCustomFile = "-f"
+const customFile = "../docker-compose.yml"
+const up = "up"
+const detachedMode = "-d"
+const logs = "logs"
+const streamMode = "-f"
+const top = "top"
+
+func serviceAlreadyRunning() bool {
+	top := exec.Command(dockerCompose, useCustomFile, customFile, top)
+	output, err := top.Output()
+	if err != nil {
+		log.Fatal("Can't run docker-compose ps: ", err)
+	}
+
+	return len(output) > 0
+}
+
 func upServer() {
-	fmt.Println("Please, be sure that you run 'make up [logs]' before start tests")
+
+	fmt.Println("Starting service...")
+	startSpinner := showSpinner()
+	upService()
+	serviceOutput := getServiceStreamOutput()
+
+	waitToServiceAlive(serviceOutput)
+
+	startSpinner.Stop()
+	fmt.Println("Service is running :)")
+}
+
+func showSpinner() *spinner.Spinner {
+	s := spinner.New(spinner.CharSets[39], 100*time.Millisecond)
+	s.Start()
+	return s
+}
+
+func getServiceStreamOutput() io.ReadCloser {
+	service := exec.Command(dockerCompose, useCustomFile, customFile, logs, streamMode)
+	serviceOutput, err := service.StdoutPipe()
+	if err != nil {
+		log.Fatalf("Can't open stream with service logs: %v\n", err)
+	}
+	err = service.Start()
+	if err != nil {
+		log.Fatalf("Can't do 'make up logs': %v\n", err)
+	}
+	return serviceOutput
+}
+
+func waitToServiceAlive(stream io.ReadCloser) {
+	serviceBuild := make(chan bool)
+	// This also can be achieved without go routines
+	go waitForDockerCompose(stream, serviceBuild)
+	isRunning := <-serviceBuild
+	if !isRunning {
+		downServer()
+		log.Fatal("Unable to start the service")
+	}
+}
+
+func upService() {
+	serviceUp := exec.Command(dockerCompose, useCustomFile, customFile, up, detachedMode)
+	err := serviceUp.Run()
+	if err != nil {
+		log.Fatalf("Can't up docker images: %v\n", err)
+	}
+}
+
+func waitForDockerCompose(serviceOutput io.ReadCloser, serviceBuild chan<- bool) {
+
+	reader := bufio.NewReader(serviceOutput)
+	go shutdownIfTimeout(serviceBuild)
+
+	for {
+		checkServiceAliveness(reader, serviceBuild)
+		return
+	}
+}
+
+func checkServiceAliveness(reader *bufio.Reader, serviceBuild chan<- bool) {
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			serviceBuild <- false
+			close(serviceBuild)
+			log.Println("EOF reached")
+			return
+		}
+
+		if err != nil {
+			log.Fatal("Read Error:", err)
+			return
+		}
+		if strings.Contains(line, "Build Failed") {
+			serviceBuild <- false
+			close(serviceBuild)
+			return
+		}
+		if strings.Contains(line, "Running...") {
+			time.Sleep(2 * time.Second)
+			serviceBuild <- true
+			close(serviceBuild)
+			return
+		}
+	}
+}
+func shutdownIfTimeout(serviceBuild chan<- bool) {
+	time.Sleep(dockerComposeTimeOut * time.Second)
+	fmt.Printf("Service don't initialized in first %v seconds\n", dockerComposeTimeOut)
+	downServer()
+	log.Fatal("Unable to start service")
 }
 
 func downServer() {
-	fmt.Println("Please,run 'make down' to shutdown the server")
+	fmt.Println("Shutting down service...")
+	serviceDown := exec.Command("docker-compose", "-f", "../docker-compose.yml", "down")
+	err := serviceDown.Run()
+	if err != nil {
+		fmt.Println("Can't shutdown service: ", err)
+	}
+	fmt.Println("Service is down")
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,15 @@ go 1.13
 
 require (
 	github.com/DATA-DOG/godog v0.7.13
+	github.com/briandowns/spinner v1.8.0
 	github.com/dgraph-io/badger/v2 v2.0.1
 	github.com/gin-gonic/gin v1.5.0
+	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/google/uuid v1.1.1
+	github.com/json-iterator/go v1.1.8 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/orov-io/BlackBart v0.0.7
 	github.com/orov-io/BlackBeard v0.0.1
 	github.com/smartystreets/goconvey v1.6.4
+	gopkg.in/go-playground/validator.v9 v9.30.0 // indirect
 )
